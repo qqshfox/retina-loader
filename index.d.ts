41daa9deb8522d5c18c0e69dd13c7cb75b9a1647
Hanfei Shen <qqshfox@gmail.com>
2018-09-10 14:53:42 +0800

    add Typescript type definitions

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+export interface Image {
+  src: string;
+  srcset: string;
+  cssImage: string;
+  cssImageSet: string;
+  width: number;
+  height: number;
+  type: string;
+}
+
+export as namespace RetinaLoader;
