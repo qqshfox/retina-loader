aecd631114d7087106435befe6d081299df68f9d
Hanfei Shen <qqshfox@gmail.com>
2018-09-11 23:02:12 +0800

    make it a dropin replacement for url-loader

--- index.js
+++ index.js
@@ -4,15 +4,10 @@ const path = require('path');
 const fs = require('fs');
 
 const DEFAULT_SCALES = ['2x', '3x'];
-const DEFAULT_URL_LOADER_OPTIONS = {
-  limit: 4096,
-  name: 'img/[name].[hash:8].[ext]',
-};
 
 module.exports = function loader(src) {
   const options = getOptions(this) || {};
   const scales = options.scales || DEFAULT_SCALES;
-  const url_loader_options = options['url-loader'] || DEFAULT_URL_LOADER_OPTIONS;
 
   const dimensions = sizeOf(this.resourcePath);
 
@@ -20,18 +15,20 @@ module.exports = function loader(src) {
 
   const urlLoader = require('url-loader');
 
+  const origin = urlLoader.call(this, src)
+
   const results = [];
   for (const scale of scales) {
     const p = path.join(pth.dir, pth.name + '@' + scale + pth.ext);
     try {
-      const result = urlLoader.call({...this, resourcePath: p, query: url_loader_options}, fs.readFileSync(p));
+      const result = urlLoader.call({...this, resourcePath: p}, fs.readFileSync(p));
       this.addDependency(p);
       results.push({scale, result});
     } catch (_err) {
     }
   }
 
-  return `${src}
+  return `${origin}
 var src = module.exports;
 var dimensions = ${JSON.stringify(dimensions)};
 
